2cbe4ec1bd6e080b666a3e27c046922c16ea9cce
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2025-05-17 03:50:14 +0530

    Refactor TypeScript types in page component and remove ESLint configuration from next.config.ts

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -4,26 +4,28 @@ import React from "react";
 import EditQues from "./EditQues";
 import { Particles } from "@/components/magicui/particles";
 
-// { params: { quesId: string; quesName: string } }
+type PageProps = {
+  params: Promise<{ quesId: string; quesName: string }>;
+};
 
-const Page = async ({ params }: { params: { quesId: string; quesName: string } }) => {
-    const question = await databases.getDocument(db, questionCollection, params.quesId);
+const Page = async ({ params }: PageProps) => {
+  const { quesId } = await params;
+  const question = await databases.getDocument(db, questionCollection, quesId);
 
-    return (
-        <div>
-            <Particles
-                className="fixed inset-0 h-full w-full"
-                quantity={500}
-                ease={100}
-                color="#ffffff"
-                refresh
-            />
-            <div className="flex justify-center items-center">
-                <EditQues question={question} />
-            </div>
-        </div>
-        
-    );
+  return (
+    <div>
+      <Particles
+        className="fixed inset-0 h-full w-full"
+        quantity={500}
+        ease={100}
+        color="#ffffff"
+        refresh
+      />
+      <div className="flex justify-center items-center">
+        <EditQues question={question} />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
